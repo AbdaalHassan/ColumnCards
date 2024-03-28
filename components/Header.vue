8482d0eab4b179be071ae9d587903c5f7a5f9270
AbdaalHassan <abdaalcss@gmail.com>
2024-03-28 16:40:19 +0500

    [Create] Column Modal

---
+++ components/Header.vue
@@ -0,0 +1,56 @@
+<template>
+  <!--  data() where is component states -->
+  <div>
+    <header class="flex justify-around items-center px-4 py-2 bg-indigo-600">
+      <h1 class="text-lg font-bold text-white">Column Cards</h1>
+      <button
+        class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+        @click="createColumn"
+      >
+        Create Column
+      </button>
+    </header>
+    <!-- Props Name (modalHeader) & (modalLabel)  -->
+    <!-- values (header) & (label)  -->
+    <!-- v-bind is a way to dynamically bind data from your Vue instance to HTML attributes or props in custom components -->
+    <!-- <a v-bind:href="url">Click me</a>  -->
+
+    <!-- v-show hide an element if the condition is false -->
+    <!-- @close event listen here and then update the state -->
+    <Modal
+      v-show="isModalVisible"
+      @close="closeModal"
+      :modalHeader="header"
+      :modalLabel="label"
+    />
+  </div>
+</template>
+
+<script>
+import Modal from "../components/Modal.vue";
+
+export default {
+  name: "Header",
+  components: {
+    Modal,
+  },
+  //   data is a function that return the object and this object contain the data properties of a component
+  data() {
+    return {
+      header: "Create a new Column",
+      label: "Column Title",
+      isModalVisible: false,
+    };
+  },
+  // methods
+  methods: {
+    createColumn() {
+      this.isModalVisible = true;
+    },
+    closeModal() {
+      this.isModalVisible = false;
+    },
+  },
+};
+</script>
+
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="bg-gray-300 h-screen">
+    <Header />
+  </div>
+</template>
+
+<script>
+import Headers from "../components/Header.vue";
+export default {
+  name: "IndexPage",
+  comments: {
+    Headers,
+  },
+};
+</script>
